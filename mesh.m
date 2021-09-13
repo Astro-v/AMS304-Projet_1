6d6a4d5731722488e5c5037f3c475cfd992f5de1
astro-v <valentinmichel07200@gmail.com>
2021-09-13 17:06:13 +0200

    Update

--- mesh.m
+++ mesh.m
@@ -1,13 +1,10 @@
-function [x,y]=mesh(a,dens,k)
-    
+function [x,y]=mesh(R,dens,k)
+    % R: rayon
+    % dens: nombre de points par lambda*
+    % k: nombre d'onde
+    % [x,y]= mesh(1,20,1)
     N = floor(dens*2*k*R+1);
-    theta = [0:0.1:2*pi];
-    x = [];
-    y = [];
-    M
-    for i = theta
-        x = [x a*cos(i)];
-        y = [y a*sin(i)];
-        
-    end
-    
+    pas=2*pi/N;
+    theta = [0:pas:2*pi];
+    x = R*cos(theta);
+    y = R*sin(theta);
