2d9edff371dafd2d9989117f1ff5b34285926efb
astro-v <valentinmichel07200@gmail.com>
2021-09-20 15:35:12 +0200

    commit

--- main.m
+++ main.m
@@ -4,8 +4,23 @@
 %%%%%%%%%               Farah CHAABA & Valentin MICHEL             %%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%% 0. Initialisation
+k = 2*pi;
+R = 1;
+dens = 20;
+
+
 %% 1. Générer un maillage du bord du disque
 
-[b,c] = mesh(1,20,1);
+[b,c] = mesh(R,dens,k);
 % plot(x,y)
 
+%% 2. Application trace
+p=[];
+for m = [1:1:size(c,2)]
+    p = [p trace(k,R,c(1,m),c(2,m))];
+end
+r = sqrt(c(1,:).^2+c(2,:).^2);
+plot(mod(real(-i.*log((c(1,:)+i.*c(2,:))./r)),2*pi),real(p))
+figure()
+plot(mod(real(-i.*log((c(1,:)+i.*c(2,:))./r)),2*pi),imag(p))
--- trace.m
+++ trace.m
@@ -1,4 +1,6 @@
-function [p]=trace(k,R,theta,r)
+function [p]=trace(k,R,x,y)
+    r = sqrt(x^2+y^2);
+    theta = -i*log((x+i*y)/r);
     n = [-100:1:100];
     p = -(k/2)*(-i).^(n).*besselj(n,k*R)./besselh(n,k*R).*(besselh(n+1,k*r)-besselh(n-1,k*r))*exp(i*theta*n)'+i*k*cos(theta)*exp(-i*k*r*cos(theta));
 end
