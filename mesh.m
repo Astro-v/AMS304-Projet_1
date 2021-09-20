87d4c9681d87084f7032128ba60f36cf8571732d
astro-v <valentinmichel07200@gmail.com>
2021-09-20 13:39:12 +0200

    Summit

--- mesh.m
+++ mesh.m
@@ -1,10 +1,14 @@
-function [x,y]=mesh(R,dens,k)
+function [x,y,T]=mesh(R,dens,k)
     % R: rayon
     % dens: nombre de points par lambda*
     % k: nombre d'onde
-    % [x,y]= mesh(1,20,1)
+    % [x,y,T]= mesh(1,20,1)
     N = floor(dens*2*k*R+1);
     pas=2*pi/N;
     theta = [0:pas:2*pi];
     x = R*cos(theta);
     y = R*sin(theta);
+    x = x(1:length(x)-1);
+    y = y(1:length(y)-1);
+    T = [[[1:1:length(x)-1];[2:1:length(y)]] [length(x);1]];
+end
