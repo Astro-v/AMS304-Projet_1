ba991f16eaf4258867105da71d974f7179ecf600
astro-v <valentinmichel07200@gmail.com>
2021-09-22 16:15:26 +0200

    commit

--- trace.m
+++ trace.m
@@ -1,5 +1,5 @@
 function [p]=trace(k,R,x,y)
-    r = sqrt(x^2+y^2);
+    r = sqrt(x^2+y^2); r
     theta = -i*log((x+i*y)/r);
     n = [-100:1:100];
     p = -(k/2)*(-i).^(n).*besselj(n,k*R)./besselh(n,k*R).*(besselh(n-1,k*r)-besselh(n+1,k*r))*exp(i*theta*n)'+i*k*cos(theta)*exp(-i*k*r*cos(theta));
